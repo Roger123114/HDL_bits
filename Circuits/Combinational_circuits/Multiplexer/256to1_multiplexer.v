71fd1eacd9cf25543555d5c33d0c66853f82c7ba
Roger123114 <roger930104@gmail.com>
2026-01-15 16:04:22 +0000

    2026/01/16

---
+++ Circuits/Combinational_circuits/Multiplexer/256to1_multiplexer.v
@@ -0,0 +1,11 @@
+module top_module( 
+    input [255:0] in,
+    input [7:0] sel,
+    output out );
+    
+    //With this many options, a case statement isn't so useful.
+	//Vector indices can be variable, as long as the synthesizer can figure out that the width of the bits being selected 
+    //is "constant". In particular, selecting one bit out of a vector using a variable index will work.
+    assign out = in[sel];
+
+endmodule
