2451b1db4548f4eec9f84a931c89794534906245
Roger123114 <roger930104@gmail.com>
2026-07-16 23:24:01 +0800

    Update Verilog practice solutions

--- Circuits/3.2Sequential_Logic/3.2.5_FSM/(g_3)Lemming3.v
+++ Circuits/3.2Sequential_Logic/3.2.5_FSM/(g_3)Lemming3.v
@@ -10,30 +10,42 @@ module top_module(
     output reg aaah,
     output reg digging ); 
 
-    parameter LEFT=2'b00,RIGHT=2'b01,groundL=2'b10,groundR=2'b11;
-    reg [1:0] state, next_state;
+    parameter LEFT=3'b000,RIGHT=3'b001,groundL=3'b010,groundR=3'b011,DIGGINGL=3'b100,DIGGINGR=3'b101;
+
+    reg [2:0] state, next_state;
 
     always @(*) begin
         // State transition logic
         case(state)
             LEFT:begin  
                 if(!ground) next_state = groundL;
+                else if(dig) next_state = DIGGINGL;
                 else if(bump_left) next_state = RIGHT;
                 else next_state = LEFT;
             end
             RIGHT:begin
                 if(!ground) next_state = groundR;
+                else if(dig) next_state = DIGGINGR;
                 else if(bump_right) next_state = LEFT;
                 else next_state = RIGHT;
             end
-            groundL:begin
+            groundL:begin //Dig ignored
                 if(ground) next_state = LEFT;
                 else next_state = groundL;
             end
-            groundR:begin
+            groundR:begin //Dig ignored
                 if(ground) next_state = RIGHT;
                 else next_state = groundR;
             end
+            DIGGINGL:begin
+                if(!ground) next_state = groundL;
+                else next_state = DIGGINGL;
+            end
+            DIGGINGR:begin
+                if(!ground) next_state = groundR;
+                else next_state = DIGGINGR;
+            end            
+
             default: next_state = LEFT;
         endcase
     end
@@ -49,43 +61,34 @@ module top_module(
     end
 
     // Output logic
-
-
     always @(posedge clk, posedge areset) begin
         if(areset)begin
-            walk_left <= 1'b1;
-            walk_right <= 1'b0;
-            aaah <= 1'b0;
+            {walk_left,walk_right,aaah,digging} <= {4'b1000};
         end
         else begin
             case(next_state)
                 LEFT:begin  
-                    walk_left <= 1'b1;
-                    walk_right <= 1'b0;
-                    aaah <= 1'b0;
+                    {walk_left,walk_right,aaah,digging} <= {4'b1000};
                 end
                 RIGHT:begin
-                    walk_left <= 1'b0;
-                    walk_right <= 1'b1;
-                    aaah <= 1'b0;
+                    {walk_left,walk_right,aaah,digging} <= {4'b0100};
                 end
                 groundL:begin
-                    walk_left <= 1'b0;
-                    walk_right <= 1'b0;
-                    aaah <= 1'b1;
+                    {walk_left,walk_right,aaah,digging} <= {4'b0010};
                 end
                 groundR:begin
-                    walk_left <= 1'b0;
-                    walk_right <= 1'b0;
-                    aaah <= 1'b1;
+                    {walk_left,walk_right,aaah,digging} <= {4'b0010};
+                end
+                DIGGINGL:begin
+                    {walk_left,walk_right,aaah,digging} <= {4'b0001};
+                end
+                DIGGINGR:begin
+                    {walk_left,walk_right,aaah,digging} <= {4'b0001};
                 end
                 default:begin
-                    walk_left <= 1'b1;
-                    walk_right <= 1'b0;
-                    aaah <= 1'b0;
+                    {walk_left,walk_right,aaah,digging} <= {4'b1000};
                 end
             endcase
         end
     end
-
 endmodule
---
+++ Circuits/3.2Sequential_Logic/3.2.5_FSM/(g_4)Lemming4.v
@@ -0,0 +1,112 @@
+module top_module(
+    input clk,
+    input areset,    // Freshly brainwashed Lemmings walk left.
+    input bump_left,
+    input bump_right,
+    input ground,
+    input dig,
+    output reg walk_left,
+    output reg walk_right,
+    output reg aaah,
+    output reg digging ); 
+
+    parameter LEFT=3'b000,RIGHT=3'b001,groundL=3'b010,groundR=3'b011,DIGGINGL=3'b100,DIGGINGR=3'b101,SPLATTER=3'b110;
+    reg [2:0] state, next_state;
+    reg [4:0] splatter_count;
+
+    always @(*) begin
+        // State transition logic
+        case(state)
+            LEFT:begin  
+                if(!ground) next_state = groundL;
+                else if(dig) next_state = DIGGINGL;
+                else if(bump_left) next_state = RIGHT;
+                else next_state = LEFT;
+            end
+            RIGHT:begin
+                if(!ground) next_state = groundR;
+                else if(dig) next_state = DIGGINGR;
+                else if(bump_right) next_state = LEFT;
+                else next_state = RIGHT;
+            end
+            groundL:begin //Dig ignored
+                if(splatter_count > 5'd20 && ground)next_state = SPLATTER;
+                else if(ground) next_state = LEFT;
+                else next_state = groundL;
+            end
+            groundR:begin //Dig ignored
+                if(splatter_count > 5'd20 && ground)next_state = SPLATTER;
+                else if(ground) next_state = RIGHT;
+                else next_state = groundR;
+            end
+            DIGGINGL:begin
+                if(!ground) next_state = groundL;
+                else next_state = DIGGINGL;
+            end
+            DIGGINGR:begin
+                if(!ground) next_state = groundR;
+                else next_state = DIGGINGR;
+            end            
+            SPLATTER:begin
+                next_state = SPLATTER;
+            end
+            default: next_state = LEFT;
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin
+        // State flip-flops with asynchronous reset
+        if(areset)begin
+            state <= LEFT;
+        end
+        else begin
+            state <= next_state;
+        end
+    end
+
+    // Output logic
+    always @(posedge clk, posedge areset) begin
+        if(areset)begin
+            {walk_left,walk_right,aaah,digging} <= {4'b1000};
+            splatter_count <= 5'b0;
+        end
+        else begin
+            case(next_state)
+                LEFT:begin  
+                    {walk_left,walk_right,aaah,digging} <= {4'b1000};
+                    splatter_count <= 5'b0;
+                end
+                RIGHT:begin
+                    {walk_left,walk_right,aaah,digging} <= {4'b0100};
+                    splatter_count <= 5'b0;
+                end
+                groundL:begin
+                    {walk_left,walk_right,aaah,digging} <= {4'b0010};
+                    if(splatter_count == 5'd21) splatter_count <= splatter_count;
+                    else splatter_count <= splatter_count + 5'd1;
+                end
+                groundR:begin
+                    {walk_left,walk_right,aaah,digging} <= {4'b0010};
+                    if(splatter_count == 5'd21) splatter_count <= splatter_count;
+                    else splatter_count <= splatter_count + 5'd1;
+                end
+                DIGGINGL:begin
+                    {walk_left,walk_right,aaah,digging} <= {4'b0001};
+                    splatter_count <= 5'b0;
+                end
+                DIGGINGR:begin
+                    {walk_left,walk_right,aaah,digging} <= {4'b0001};
+                    splatter_count <= 5'b0;
+                end
+                SPLATTER:begin
+                    {walk_left,walk_right,aaah,digging} <= {4'b0000};
+                    splatter_count <= splatter_count;
+                end
+                default:begin
+                    {walk_left,walk_right,aaah,digging} <= {4'b1000};
+                    splatter_count <= 5'b0;
+                end
+            endcase
+        end
+    end
+endmodule
